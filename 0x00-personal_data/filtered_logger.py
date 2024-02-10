94d6b6effa54499343890d503808ff8c31220839
TonnyMpho <tonnympho1@gmail.com>
2024-02-10 11:51:48 +0000

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,30 @@ import logging
 from typing import List
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        method to filter values in incoming log records using filter_datum
+        """
+        message = super(RedactingFormatter, self).format(record)
+        redacted = filter_datum(self.fields, self.REDACTION, message,
+                                self.SEPARATOR)
+        return redacted
+
+
 def filter_datum(fields: List[str], redaction: str,
-                message: str, separator: str) -> str:
+                 message: str, separator: str) -> str:
     """
     function that returns the log message obfuscated:
     ------------------
@@ -17,4 +39,5 @@ def filter_datum(fields: List[str], redaction: str,
     separator: a string representing by which character is separating
     all fields in the log line (message)
     """
-    return re.sub(fr'({"|".join(fields)})=[^{separator}]+', f'\\1={redaction}', message)
+    return re.sub(fr'({"|".join(fields)})=[^{separator}]+',
+                  f'\\1={redaction}', message)
