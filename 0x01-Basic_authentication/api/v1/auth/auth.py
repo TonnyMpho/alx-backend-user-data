f7533811906d33286d3d1ce283ce5d9b0dcc298b
TonnyMpho <tonnympho1@gmail.com>
2024-02-13 18:20:47 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from typing import List, TypeVar
 class Auth:
     """ 3. Auth class
     """
-    def require(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ method that returns False
         """
         return False
