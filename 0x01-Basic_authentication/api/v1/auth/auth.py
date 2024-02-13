54d9e1b6f4f5e61fedf8f4bc3fe13d8bb6e3def8
TonnyMpho <tonnympho1@gmail.com>
2024-02-13 18:25:43 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,12 +8,14 @@ class Auth:
     """ 3. Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ method that returns False
+        """ method that reqiure authorization
         """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ method that checks authorization headers """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ method that checks if the user is authorized """
         return None
