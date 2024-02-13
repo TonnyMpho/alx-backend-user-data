6df7cf3c38cdd9f03dd4e2d9f6a9f82ecf29ec9f
TonnyMpho <tonnympho1@gmail.com>
2024-02-13 18:16:13 +0000

    Basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" API authentication. """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ 3. Auth class
+    """
+    def require(self, path: str, excluded_paths: List[str]) -> bool:
+        """ method that returns False
+        """
+        return False
+
+    def def authorization_header(self, request=None) -> str:
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
