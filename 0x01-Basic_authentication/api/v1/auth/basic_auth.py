ad25e812a56ba7cbb98f9cd3f72610875dda85d0
TonnyMpho <tonnympho1@gmail.com>
2024-02-14 02:33:38 +0000

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,14 +68,17 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        users = User.search({'email': user_email})
-        if not users or users == []:
-            return None
+        try:
+            users = User.search({'email': user_email})
+            if not users or users == []:
+                return None
 
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
