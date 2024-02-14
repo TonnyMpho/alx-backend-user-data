89e66b44b3a3da948ecf583ab2dd2e490f346938
TonnyMpho <tonnympho1@gmail.com>
2024-02-14 18:40:29 +0000

    Session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,9 +43,11 @@ def before_request():
             ]
     request.current_user = auth.current_user(request)
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.session_cookie(request) is None:
+
+        auth_header = auth.authorization_header(request)
+        session_cookie = auth.session_cookie(request)
+
+        if auth_header is None and session_cookie is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
