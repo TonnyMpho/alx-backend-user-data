956352c643778234cbee583d4e44eacc976a80d7
TonnyMpho <tonnympho1@gmail.com>
2024-02-20 20:31:38 +0000

    User authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,10 +34,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """method that returns a User object
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
         self._session.commit()
-        return user
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """ returns a user found, filtered by the input arguments
