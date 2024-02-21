7dff4eb6d611694e611c809570e66ed5200cea5e
TonnyMpho <tonnympho1@gmail.com>
2024-02-21 21:19:52 +0000

    User authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,7 +36,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            hashed_password = user.hashed_password
+            return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
         except Exception:
             return False
-
