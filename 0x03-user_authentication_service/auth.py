108f0ae426d06a14f5e7d82a67d5f8f42dea3f1f
TonnyMpho <tonnympho1@gmail.com>
2024-02-21 21:11:49 +0000

    User authentication service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,13 @@ class Auth:
             new_user = self._db.add_user(email, _hash_password(password))
             return new_user
         raise ValueError('User {} already exists'.format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """method that checks if the password is correct
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except Exception:
+            return False
+
