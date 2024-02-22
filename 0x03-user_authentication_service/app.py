e2eeb34330cc03df28f3a6fd856576e9343c6d9e
TonnyMpho <tonnympho1@gmail.com>
2024-02-22 13:52:00 +0000

    User authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,7 +28,7 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/session', methods=['POST'], strict_slashes=False)
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login():
     """login a user and create a session
     """
